405f30d8c5beaae82ae5bfe1004d72bac3323896
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2022-10-30 16:02:33 +0530

    one form added with get method

--- djangoherokuRefresh/views.py
+++ djangoherokuRefresh/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def home(request):
     return render(request, 'djangoherokurefresh/index.html')
+
+
+def view(request):
+    print(request.GET.get("FirstName"))
+    return HttpResponse("done")
